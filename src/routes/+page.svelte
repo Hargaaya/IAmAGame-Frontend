0756f6a66a52d70fd69463ced82cac5df8f35c91
Hargaaya <hargaaya10@gmail.com>
2023-02-05 19:39:58 +0100

    use gallery components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Gallery from '../components/FrontpageGallery/Gallery.svelte';
+	import GalleryItem from '../components/FrontpageGallery/GalleryItem.svelte';
+</script>
+
+<Gallery>
+	<GalleryItem
+		slot="gallery-featured"
+		title="Gif Party"
+		image="./images/black_balloons.jpg"
+		featured={true}
+	/>
+	<GalleryItem title="Whose Lie Is It Anyway?" image="./images/purple_imps.jpg" />
+	<GalleryItem title="Trivia Clash" image="./images/people_laughing.jpg" />
+	<GalleryItem title="Word Twist" image="./images/multicolor_wool.jpg" />
+</Gallery>
