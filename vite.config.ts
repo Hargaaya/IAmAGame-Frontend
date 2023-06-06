eb4e83c56c0567a2c4bd2b3cabd957af0b3caae8
Jacob Klaren <33457831+Kybbe@users.noreply.github.com>
2023-06-06 15:45:03 +0200

    misc stuff launching

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,17 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+    watch: {
+      usePolling: true,
+    },
+    port: 8080,
+    host: true,
+  },
+	preview: {
+    port: 8080,
+  },
 };
 
 export default config;
